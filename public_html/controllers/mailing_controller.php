f142b925e4f386437131234676a314abe98ee2fd
Novichkov <novichvkovv@bk.ru>
2015-10-01 11:02:36 +0300

    No Message
    Commit with no message
    
    Signed-off-by: Novichkov <novichvkovv@bk.ru>

--- public_html/controllers/mailing_controller.php
+++ public_html/controllers/mailing_controller.php
@@ -12,22 +12,25 @@ class mailing_controller extends controller
         if($users = $this->model('mailing')->getUsers()) {
             $mailing_data = $this->model('mailing')->getDailyMailingData();
             $i = 0;
+            //print_r($users);
             foreach($users as $k => $user) {
-                if($i == 100) {
-                    break;
-                }
                 $date = date('Y-m-d 05:00:00', strtotime($user['sdate']));
                 $day = date_diff(new DateTime(), new DateTime($date))->days;
-
                 if($day == 0) {
                     continue;
                 }
                 if($user['sent'] >= $day)continue;
-                $i ++;
                 $data = $mailing_data[$day];
+                if($i == 100) {
+                    break;
+                }
                 if($data['subject']) {
                     $this->sendEmail($day, $user, $data);
+                } else {
+                    continue;
                 }
+                $i ++;
+
             }
 
         }
